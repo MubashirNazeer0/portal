11e2c7c2e600a7454436a2b7f179f1e8aa61f0e3
MubashirNazeer <mubashirnazeeer50@gmail.com>
2024-11-24 20:45:44 +0500

    logout and minor issue sresolved

--- static/js/caro.js
+++ static/js/caro.js
@@ -5,3 +5,16 @@ $('#button-right').click(function() {
 $('#button-left').click(function() {
   $('.slider').children().addClass('right').removeClass('left').last().prependTo('.slider');
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute('href'));
+      if (target) {
+          target.scrollIntoView({
+              behavior: 'smooth',
+              block: 'start'
+          });
+      }
+  });
+});
